32f90d358f5b0f419d81221e0a297fe70820e75d
Thiagonogsl <thiagonsilva06@gmail.com>
2025-12-19 18:46:48 -0300

    Create Register user UseCase

--- src/backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
@@ -12,7 +13,11 @@ namespace MyRecipeBook.API.Controllers
         [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
         public IActionResult Register(RequestRegisterUserJson request)
         {
-            return Created();
+            var useCase = new RegisterUserUseCase();
+
+            var result = useCase.Execute(request);
+
+            return Created(string.Empty ,result);
         }
     }
 }
---
+++ src/backend/MyRecipeBook.Application/UseCases/User/RegisterUserUseCase.cs
@@ -0,0 +1,23 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.User
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+            // Validate request
+
+            //Mapear a request em uma entidade
+
+            //Criptografar a senha
+
+            //Salvar a entidade no banco de dados
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name
+            };
+        }
+    }
+}
