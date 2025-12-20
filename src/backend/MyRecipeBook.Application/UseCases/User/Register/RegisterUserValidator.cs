a06ed301273511399448287ad9adddd7cf68edb4
Thiagonogsl <thiagonsilva06@gmail.com>
2025-12-19 21:08:57 -0300

    Create the 'ResourceMessageExceptions' file, which customizes the error messages

--- src/backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -7,10 +7,10 @@ namespace MyRecipeBook.Application.UseCases.User.Register
     {
         public RegisterUserValidator()
         {
-            RuleFor(user => user.Name).NotEmpty();
-            RuleFor(user => user.Email).NotEmpty();
-            RuleFor(user => user.Email).EmailAddress();
-            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+            RuleFor(user => user.Name).NotEmpty().WithMessage("Name cannot be empty");
+            RuleFor(user => user.Email).NotEmpty().WithMessage("Email cannot be empty");
+            RuleFor(user => user.Email).EmailAddress().WithMessage("Invalid email addres");
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage("Password must be greater than 6 characters");
         }
     }
 }
