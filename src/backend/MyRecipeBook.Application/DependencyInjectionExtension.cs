52cec5597d0d767e0f2b1df00c766212562401ed
Thiagonogsl <thiagonsilva06@gmail.com>
2025-12-21 17:04:07 -0300

    Configurate dependency injection on Infrastructure project

---
+++ src/backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+namespace MyRecipeBook.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplicationDependencies(this IServiceCollection services)
+        {
+            // Add application related dependencies here
+            AddRepositories(services);
+            AddDbContext_MySql(services);
+        }
+
+        private static void AddDbContext_MySql(IServiceCollection services)
+        {
+            var connectionString = "Server=localhost;Database=meulivrodereceitas;Uid=root;Pwd=@Password123";
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 44));
+
+            services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions =>
+            {
+                dbContextOptions.UseMySql(connectionString, serverVersion);
+            });
+                
+        }
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+            services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        }
+    }
+}
---
+++ src/backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MyRecipeBook.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructureDependencies(this IServiceCollection services)
+        {
+            // Add infrastructure related dependencies here
+        }
+    }
+}
