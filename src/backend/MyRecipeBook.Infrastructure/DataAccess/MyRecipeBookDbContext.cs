db0953ff3cc0b4ff5b49680ac5dfc4e3d146d066
Thiagonogsl <thiagonsilva06@gmail.com>
2025-12-21 15:31:53 -0300

    Create DbContext using EntityFramework

---
+++ src/backend/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAccess
+{
+    public class MyRecipeBookDbContext : DbContext
+    {
+        //Cria um construtor que recebe as opções como parametro e repassa para a classe base DbContext
+        public MyRecipeBookDbContext(DbContextOptions<MyRecipeBookDbContext> options): base(options)
+        {
+        }
+
+        //Avisa para o Entity Framework que existe uma tabela Users no banco de dados
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyRecipeBookDbContext).Assembly);
+        }
+    }
+}
