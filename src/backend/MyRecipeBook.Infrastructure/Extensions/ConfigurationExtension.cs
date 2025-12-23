80dd64b8100ca5b9f237df99b7a822355d7a9feb
Thiagonogsl <thiagonsilva06@gmail.com>
2025-12-23 12:00:17 -0300

    Create Database via API

---
+++ src/backend/MyRecipeBook.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyRecipeBook.Infrastructure.Extensions
+{
+    public static class ConfigurationExtension
+    {
+        public static string ConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetConnectionString("DefaultConnection")!;
+        }
+    }
+}
---
+++ src/backend/MyRecipeBook.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace MyRecipeBook.Infrastructure.Migrations
+{
+    public static class DataBaseMigration
+    {
+        public static void Migrate(string connectionString)
+        {
+            var connectionStringBuilder = new MySqlConnectionStringBuilder(connectionString);
+            var databaseName = connectionStringBuilder.Database;
+
+            connectionStringBuilder.Remove("Database");
+
+            using var dbConnection = new MySqlConnection(connectionStringBuilder.ConnectionString);
+
+            var parameters = new DynamicParameters();
+            parameters.Add("name", databaseName);
+
+            var records = dbConnection.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @name", parameters);
+
+            if (records.Any() == false)
+                dbConnection.Execute($"CREATE DATABASE {databaseName}");
+        }
+    }
+}
