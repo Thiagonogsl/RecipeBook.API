cc29c0310023d16a157a6f6f5657bffeba444208
Thiagonogsl <thiagonsilva06@gmail.com>
2025-12-29 22:24:40 -0300

    Create User table using Fluent Migrator

--- src/backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version000001.cs
+++ src/backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version000001.cs
@@ -3,11 +3,17 @@
 namespace MyRecipeBook.Infrastructure.Migrations.Versions
 {
     [Migration(1, "Create table to save user information")]
-    public class Version000001 : ForwardOnlyMigration
+    public class Version000001 : VersionBase
     {
         public override void Up()
         {
-
+            Create.Table("Users")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("CreatedOn").AsDateTime().NotNullable()
+                .WithColumn("Active").AsBoolean().NotNullable()
+                .WithColumn("Name").AsString(255).NotNullable()
+                .WithColumn("Email").AsString(255).NotNullable()
+                .WithColumn("Password").AsString(2000).NotNullable();
         }
     }
 }
