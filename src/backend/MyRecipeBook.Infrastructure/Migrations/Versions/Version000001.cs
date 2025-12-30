209bfd9456274be7d12358e3c5d2c7b9bdff1c7f
Thiagonogsl <thiagonsilva06@gmail.com>
2025-12-29 22:13:05 -0300

    Initial Fluent Migrator configuration

---
+++ src/backend/MyRecipeBook.Infrastructure/Migrations/Versions/Version000001.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator;
+
+namespace MyRecipeBook.Infrastructure.Migrations.Versions
+{
+    [Migration(1, "Create table to save user information")]
+    public class Version000001 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+
+        }
+    }
+}
