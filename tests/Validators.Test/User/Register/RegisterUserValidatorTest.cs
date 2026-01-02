fd0bb19251ea930057124a0d5b6c706f084a8e67
Thiagonogsl <thiagonsilva06@gmail.com>
2026-01-02 14:50:21 -0300

    Start creating register user validator test

---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,17 @@
+﻿using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace Validators.Test.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterUserValidator();
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid == true;
+        }
+    }
+}
