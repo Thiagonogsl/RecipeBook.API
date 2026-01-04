3ba00543d4eb052ea847f21638507858b3d2d28f
Thiagonogsl <thiagonsilva06@gmail.com>
2026-01-03 23:40:22 -0300

    Create unity tests for error messages(Name_Empty, Email_Empty and Email_Invalid)

--- tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -1,5 +1,6 @@
 ﻿using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
 using Shouldly;
 
 namespace Validators.Test.User.Register
@@ -16,8 +17,60 @@ namespace Validators.Test.User.Register
 
             //Assert
             result.IsValid.ShouldBeTrue();
+        }
 
-            //result.IsValid.Should().BeTrue();
+        [Fact]
+        public void Error_Name_Empty()
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse(); //Garante que ocorreu um erro
+            result.Errors.ShouldSatisfyAllConditions(
+                e => e.ShouldHaveSingleItem(),
+                e => e.ShouldContain(m => m.ErrorMessage.Equals(ResourceMessagesException.NAME_EMPTY))
+            ); 
+        }
+
+        [Fact]
+        public void Error_Email_Empty()
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = string.Empty;
+
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse(); 
+            result.Errors.ShouldSatisfyAllConditions(
+                e => e.ShouldHaveSingleItem(),
+                e => e.ShouldContain(m => m.ErrorMessage.Equals(ResourceMessagesException.EMAIL_EMPTY))
+            );
+        }
+
+        [Fact]
+        public void Error_Email_Invalid()
+        {
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Email = "email.com";
+
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeFalse();
+            result.Errors.ShouldSatisfyAllConditions(
+                e => e.ShouldHaveSingleItem(),
+                e => e.ShouldContain(m => m.ErrorMessage.Equals(ResourceMessagesException.EMAIL_INVALID))
+            );
         }
     }
 }
